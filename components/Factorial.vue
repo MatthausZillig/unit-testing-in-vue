5a11ecbf42e05867afbaef8dadf8ecc9021372fa
MatthausZillig <matthauszigillig@hotmail.com>
2022-06-12 01:25:29 -0300

    feat: factorial component

---
+++ components/Factorial.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>{{ result }}</span>
+    <button @click="getFactorial">Get Factorial</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FactorialComponent',
+  props: {
+    number: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      result: 1,
+    }
+  },
+  methods: {
+    getFactorial() {
+      // eslint-disable-next-line vue/no-mutating-props
+      while (this.number > 0) this.result *= this.number--
+      return this.result
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <factorial-component :number="13" />
+</template>
+
+<script lang="ts">
+import FactorialComponent from '~/components/Factorial.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    FactorialComponent,
+  },
+}
+</script>
